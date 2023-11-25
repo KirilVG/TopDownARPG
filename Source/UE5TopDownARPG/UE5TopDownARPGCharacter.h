1a0483223d5ca1457fa1853b965ec714f446aed0
Kiril Gradinarov <kirilgradinarov6@gmail.com>
2023-11-25 15:08:44 +0200

    Initialize the repo

---
+++ Source/UE5TopDownARPG/UE5TopDownARPGCharacter.h
@@ -0,0 +1,61 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "UE5TopDownARPGCharacter.generated.h"
+
+UCLASS(Blueprintable)
+class AUE5TopDownARPGCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AUE5TopDownARPGCharacter();
+
+	// Called every frame.
+	virtual void BeginPlay() override;
+
+	// Called every frame.
+	virtual void Tick(float DeltaSeconds) override;
+
+	/** Returns TopDownCameraComponent subobject **/
+	FORCEINLINE class UCameraComponent* GetTopDownCameraComponent() const { return TopDownCameraComponent; }
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+
+	bool ActivateAbility(FVector Location, int AbilityIndex);
+
+private:
+	/** Top down camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* TopDownCameraComponent;
+
+	/** Camera boom positioning the camera above the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY()
+	TArray<class UBaseAbility*> AbilityInstances;
+
+	UPROPERTY(EditDefaultsOnly)
+	TArray<TSubclassOf<class UBaseAbility>> AbilityTemplates;
+
+	UPROPERTY(EditDefaultsOnly)
+	float Health = 100.0f;
+
+	UPROPERTY(EditDefaultsOnly)
+	float DeathDelay = 1.0f;
+
+	FTimerHandle DeathHandle;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AActor> AfterDeathSpawnClass;
+
+	UFUNCTION()
+	void TakeAnyDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigateBy, AActor* DamageCauser);
+
+	void Death();
+};
+
